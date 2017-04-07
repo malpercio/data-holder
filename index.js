c00a4375f18bb9001276ac4d88a3535223b8189a
Daniel González Espinoza <danielglezespinoza@gmail.com>
2017-04-06 20:46:53 -0500

    Added DoubleSyncedList

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ module.exports = (promiseLibrary) => {
     SyncLinkedList: require('./src/list/SyncLinkedList')(promiseLibrary),
     LinkedList: require('./src/list/LinkedList')(promiseLibrary),
     SyncDoubleLinkedList: require('./src/list/SyncDoubleLinkedList')(promiseLibrary),
-    SyncList: require('./src/list/SyncDoubleLinkedList')(promiseLibrary)
+    SyncList: require('./src/list/SyncDoubleLinkedList')(promiseLibrary),
+    DoubleLinkedList: require('./src/list/DoubleLinkedList')(promiseLibrary),
+    List: require('./src/list/DoubleLinkedList')(promiseLibrary)
   }
 };
