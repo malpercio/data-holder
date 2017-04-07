0926832897fc70338bf8c39a0200a30f325efd13
Daniel González Espinoza <danielglezespinoza@gmail.com>
2017-04-07 16:28:54 -0500

    Removed unnecessary sync code. My bad, I didn't realized it sooner. :(

--- test/structures/list/DoubleLinkedList.test.js
+++ test/structures/list/DoubleLinkedList.test.js
@@ -1,83 +1,68 @@
 let assert = require('assert');
 let faker = require('faker');
-let times = require('async/times');
 
 let numberOfItems = faker.random.number()%100 + 1;
 let arbitraryElement = 'Bunny';
 
-let dataHolder = require('../../../index.js')();
+let dataHolder = require('../../../index.js')('Sync');
 let List = dataHolder.DoubleLinkedList;
 
 let genericTests = require('../../lib/genericList.test')('DoubleLinkedList');
 
-describe('DoubleLinkedList specific behaviour', () => {
+describe('SyncDoubleLinkedList specific behaviour', () => {
 
     it('should link backwards when adding', (done) => {
       let testingList = new List(),
         i;
-      times(numberOfItems, (i, next) => {
-        testingList.add(i, (err) => {
-          if (err){
-            return next(err);
-          }
-          if (testingList.tail.prev && testingList.tail.prev.value != i - 1){
-            return next(new Error('Previous element not found'));
-          }
-          return next();
-        });
-      }, done);
+      for (i=0; i <= numberOfItems; i++){
+        testingList.add(i);
+        if (testingList.tail.prev && testingList.tail.prev.value != i - 1){
+          return done(new Error('Previous element not found'));
+        }
+      }
+      done();
     });
 
     it('should link backwards when popping', (done) => {
       let testingList = new List(),
         i;
-      times(numberOfItems, (i, next) => {testingList.add(i, next)}, () => {
-        times(numberOfItems, (i, next) => {
-          testingList.pop((err) => {
-            if (err){
-              return next(err);
-            }
-            if (testingList.tail && testingList.tail.value != numberOfItems - i - 2 ){
-              return next(new Error('Previous element not found'));
-            }
-            return next();
-          });
-        }, done);
-      });
+      for (i=0; i <= numberOfItems; i++){
+        testingList.add(i);
+        }
+      for (i=0; i <= numberOfItems; i++){
+        testingList.pop();
+        if (testingList.tail && testingList.tail.value != numberOfItems - i - 1 ){
+          return done(new Error('Previous element not found'));
+        }
+      }
+      done();
     });
 
     it('should link backwards when shifting', (done) => {
       let testingList = new List(),
         i;
-      times(numberOfItems, (i, next) => {testingList.add(i, next)}, () => {
-        times(numberOfItems, (i, next) => {
-          testingList.shift((err) => {
-            if (err){
-              return next(err);
-            }
-            if (testingList.head && testingList.head.value != i + 1){
-              return next(new Error('Previous element not found'));
-            }
-            return next();
-          });
-        }, done);
-      });
+      for (i=0; i <= numberOfItems; i++){
+        testingList.add(i);
+        }
+      for (i=0; i <= numberOfItems; i++){
+        testingList.shift();
+        if (testingList.head && testingList.head.value != i +1 ){
+          return done(new Error('Previous element not found'));
+        }
+      }
+      done();
     });
 
     it('should link backwards when unshifting', (done) => {
       let testingList = new List(),
         i;
-      times(numberOfItems, (i, next) => {
-        testingList.unshift(i, (err) => {
-          if (err){
-            return next(err);
-          }
-          if (testingList.head.next && testingList.head.next.prev.value != i){
-            return next(new Error('Previous element not found'));
-          }
-          return next();
-        });
-      }, done);
+      for (i=0; i <= numberOfItems; i++){
+        testingList.unshift(i);
+        if (testingList.head.next && testingList.head.next.prev.value != i){
+          return done(new Error('Previous element not found'));
+        }
+      }
+      done();
     });
 
     it('should link backwards when splicing', (done) => {
@@ -85,22 +70,20 @@ describe('DoubleLinkedList specific behaviour', () => {
         i,
         j = 0,
         savedNode;
-      times(numberOfItems, (i, next) => {testingList.add(i, next)}, () => {
-        times(numberOfItems, (i, next) => {
-          testingList.splice(1, (err) => {
-            if (err){
-              return next(err);
-            }
-            for (savedNode = testingList.tail; j < testingList.length; savedNode = savedNode.prev){
-              j++;
-            }
-            if (savedNode.element != testingList.head.element){
-              return next(new Error('Previous element not found'));
-            }
-            return next();
-          });
-        }, done);
-      });
+      for (i=0; i <= numberOfItems; i++){
+        testingList.add(i);
+        }
+      for (i=1; i <= numberOfItems; i++){
+        testingList.splice(1);
+        savedNode = this.tail;
+        for (savedNode = testingList.tail; j < testingList.length; savedNode = savedNode.prev){
+          j++;
+        }
+        if (savedNode.element != testingList.head.element){
+          return done(new Error('Previous element not found'));
+        }
+      }
+      done();
     });
 
 });
