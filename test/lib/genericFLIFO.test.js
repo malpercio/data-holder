730192b66f3b9de7683f874fa9856e6d17d65f6d
Daniel González Espinoza <danielglezespinoza@gmail.com>
2017-04-10 18:07:10 -0500

    Reworked FLIFO tests

---
+++ test/lib/genericFLIFO.test.js
@@ -0,0 +1,83 @@
+module.exports = function(implementation){
+  let assert = require('assert');
+  let faker = require('faker');
+
+  let numberOfItems = faker.random.number()%100 + 1;
+
+  let dataHolder = require('../../index.js')('Sync');
+  let FLIFO = dataHolder[implementation];
+
+  function isEmpty (testingFLIFO, done){
+    if(testingFLIFO.length != 0){
+      return done(new Error('FLIFO has a non zero initial length'));
+    }
+    if (testingFLIFO.elements[0]){
+      return done(new Error('FLIFO has at least one element.'));
+    }
+    done();
+  }
+
+  describe('Generic FLIFO behaviour', () => {
+
+    it('should create an object', function(done){
+      let testingList = new FLIFO();
+      if (typeof testingList == 'object' && testingList instanceof FLIFO){
+        return done();
+      }
+      return done(new Error('Not an object'));
+    });
+
+    it('should add at bottom', (done) => {
+      let testingFLIFO = new FLIFO(),
+        i;
+      for (i = 1; i <= numberOfItems; i++){
+        testingFLIFO.push(i);
+        if (testingFLIFO.length != i){
+          return done(new Error('Length does not decrease when adding elements'));
+        }
+        if(testingFLIFO.elements[i - 1] != i){
+          return done(new Error('Element not found at bottom'));
+        }
+      }
+      done();
+    });
+
+    it('should have next item while not empty', (done) => {
+      let testingFLIFO = new FLIFO(),
+      i;
+      for (i = 0; i <= numberOfItems; i++){
+        testingFLIFO.push(i);
+        if (!testingFLIFO.hasNext()){
+          return done(new Error('Elements not found'));
+        }
+      }
+      for (i = numberOfItems; i >= 1; i--){
+        testingFLIFO.pop();
+        if (!testingFLIFO.hasNext()){
+          return done(new Error('Element not found'));
+        }
+      }
+      testingFLIFO.pop();
+      if(testingFLIFO.hasNext()){
+        return done(new Error('Element found'));
+      }
+      done();
+    });
+
+    it('should empty FLIFO', (done) => {
+      let testingFLIFO = new FLIFO(),
+        i;
+      for (i = 1; i <= numberOfItems; i++){
+        testingFLIFO.push(i);
+      }
+      testingFLIFO.empty();
+      isEmpty(testingFLIFO, done);
+    });
+
+    it('should be empty at creation', (done) => {
+      let testingFLIFO = new FLIFO();
+      isEmpty(testingFLIFO, done);
+    });
+
+  });
+}
--- test/structures/flifo/stack.test.js
+++ test/structures/flifo/stack.test.js
@@ -7,40 +7,9 @@ let arbitraryElement = 'Bunny';
 let dataHolder = require('../../../index.js')('Sync');
 let Stack = dataHolder.Stack;
 
-function isEmpty (testingStack, done){
-  if(testingStack.length != 0){
-    return done(new Error('Stack has a non zero initial length'));
-  }
-  if (testingStack.elements[0]){
-    return done(new Error('Stack has at least one element.'));
-  }
-  done();
-}
+let genericTests = require('../../lib/genericFLIFO.test')('Stack');
 
-describe('Stack behaviour', () => {
-
-  it('should create an object', function(done){
-    let testingList = new Stack();
-    if (typeof testingList == 'object' && testingList instanceof Stack){
-      return done();
-    }
-    return done(new Error('Not an object'));
-  });
-
-  it('should add at bottom', (done) => {
-    let testingStack = new Stack(),
-      i;
-    for (i = 1; i <= numberOfItems; i++){
-      testingStack.push(i);
-      if (testingStack.length != i){
-        return done(new Error('Length does not increase when adding elements'));
-      }
-      if(testingStack.elements[i - 1] != i){
-        return done(new Error('Element not found at bottom'));
-      }
-    }
-    done();
-  });
+describe('Specific Stack behaviour', () => {
 
   it('should remove from bottom', (done) => {
     let testingStack = new Stack(),
@@ -58,43 +27,6 @@ describe('Stack behaviour', () => {
       }
     }
     done();
-  });
-
-  it('should have next item while not empty', (done) => {
-    let testingStack = new Stack(),
-    i;
-    for (i = 0; i <= numberOfItems; i++){
-      testingStack.push(i);
-      if (!testingStack.hasNext()){
-        return done(new Error('Elements not found'));
-      }
-    }
-    for (i = numberOfItems; i >= 1; i--){
-      testingStack.pop();
-      if (!testingStack.hasNext()){
-        return done(new Error('Element not found'));
-      }
-    }
-    testingStack.pop();
-    if(testingStack.hasNext()){
-      return done(new Error('Element found'));
-    }
-    done();
-  });
-
-  it('should empty stack', (done) => {
-    let testingStack = new Stack(),
-      i;
-    for (i = 1; i <= numberOfItems; i++){
-      testingStack.push(i);
-    }
-    testingStack.empty();
-    isEmpty(testingStack, done);
-  });
-
-  it('should be empty at creation', (done) => {
-    let testingStack = new Stack();
-    isEmpty(testingStack, done);
   });
 
 });
